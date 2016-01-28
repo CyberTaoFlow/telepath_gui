b31713c6c86a025d942124a0f7078639d21a7845
Yossi Cahen <yossi@ravtech.co.il>
2016-01-28 13:18:24 +0200

    drag n drop - improve the code

--- application/controllers/config.php
+++ application/controllers/config.php
@@ -117,7 +117,7 @@ class Config extends Tele_Controller
         $this->load->model('M_Config');
 
 
-            return_json(array('scheduler' => $this->M_Config->get_scheduler(), 'success' => true));
+        return_json(array('scheduler' => $this->M_Config->get_scheduler(), 'success' => true));
 
 
     }
@@ -226,7 +226,7 @@ class Config extends Tele_Controller
 
         if (isset($config['whitelist'])) {
 
-            $whitelist=$this->M_Config->whitelist_set_ips($config['whitelist']);
+            $whitelist = $this->M_Config->whitelist_set_ips($config['whitelist']);
 
 //			#$whitelist_new = explode(',', $config['ip_whitelist']);
 //			$whitelist_new = $config['whitelist'];
@@ -245,24 +245,22 @@ class Config extends Tele_Controller
 
         if (isset($config['scheduler'])) {
 
-             $this->M_Config->set_scheduler($config['scheduler']);
+            $this->M_Config->set_scheduler($config['scheduler']);
 
 
-        }
-        else
-        {
+        } else {
             $this->M_Config->set_scheduler(array());
         }
 
         if (isset($config['ip_balances'])) {
 
-            $ip_balances=$this->M_Config->set_ip_balances($config['ip_balances']);
+            $ip_balances = $this->M_Config->set_ip_balances($config['ip_balances']);
             //var_dump($ip_balances);
         }
 
         if (isset($config['header_balances'])) {
 
-            $header_balances=$this->M_Config->set_header_balances($config['header_balances']);
+            $header_balances = $this->M_Config->set_header_balances($config['header_balances']);
             //var_dump($header_balances);
         }
 
@@ -292,16 +290,15 @@ class Config extends Tele_Controller
                 case 'app_list_was_changed_id':
 
 
-                    $config_response=$this->M_Config->update($key, $value);
-                //var_dump($config_response);
+                    $config_response = $this->M_Config->update($key, $value);
+                    //var_dump($config_response);
 
-                break;
+                    break;
             }
         }
 
-     //   $config_response=$this->M_Config->update('app_list_was_changed_id', '1');
+        //   $config_response=$this->M_Config->update('app_list_was_changed_id', '1');
         //var_dump($config_response);
-
 
 
         /*        foreach ($config as $key => $value) {
@@ -342,7 +339,7 @@ class Config extends Tele_Controller
 
                 }*/
         // set ignore extensions, Yuli
-        $regex =$this->M_Config->set_regex($config['regex']['URL']);
+        $regex = $this->M_Config->set_regex($config['regex']['URL']);
         //var_dump($regex);
 
         /*
@@ -381,7 +378,7 @@ class Config extends Tele_Controller
 
 //				system('/opt/telepath/suricata/af-packet.sh &');
 //			}
-            $agents=$this->M_Config->set_agents($config['agents']);
+            $agents = $this->M_Config->set_agents($config['agents']);
             //var_dump($agents);
 
         }
@@ -392,55 +389,59 @@ class Config extends Tele_Controller
 
 
     }
+
     public function do_upload()
     {
 
-    $base_target_file= FCPATH. 'upload/';
-    $target_file = $base_target_file .basename($_FILES["file"]["name"]);
+            $base_target_file = FCPATH . 'upload/';
+            $target_file = $base_target_file . basename($_FILES["file"]["name"]);
 //        $uploadOk = 1;
 //        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-        // Check if image file is a actual image or fake image
-        /*       if (isset($_POST["submit"])) {
-                   $check = getimagesize($_FILES["file"]["tmp_name"]);
-                   if ($check !== false) {
-                       echo "File is an image - " . $check["mime"] . ".";
-                       $uploadOk = 1;
-                   } else {
-                       echo "File is not an image.";
+            // Check if image file is a actual image or fake image
+            /*       if (isset($_POST["submit"])) {
+                       $check = getimagesize($_FILES["file"]["tmp_name"]);
+                       if ($check !== false) {
+                           echo "File is an image - " . $check["mime"] . ".";
+                           $uploadOk = 1;
+                       } else {
+                           echo "File is not an image.";
+                           $uploadOk = 0;
+                       }
+                   }
+           // Check if file already exists
+                   if (file_exists($target_file)) {
+                       echo "Sorry, file already exists.";
                        $uploadOk = 0;
                    }
-               }
-       // Check if file already exists
-               if (file_exists($target_file)) {
-                   echo "Sorry, file already exists.";
-                   $uploadOk = 0;
-               }
-       // Check file size
-               if ($_FILES["fileToUpload"]["size"] > 500000) {
-                   echo "Sorry, your file is too large.";
-                   $uploadOk = 0;
-               }
-       // Allow certain file formats
-               if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                   && $imageFileType != "gif"
-               ) {
-                   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                   $uploadOk = 0;
-               }
-       // Check if $uploadOk is set to 0 by an error
-               if ($uploadOk == 0) {
-                   echo "Sorry, your file was not uploaded.";
-       // if everything is ok, try to upload file
-               } else {*/
+           // Check file size
+                   if ($_FILES["fileToUpload"]["size"] > 500000) {
+                       echo "Sorry, your file is too large.";
+                       $uploadOk = 0;
+                   }
+           // Allow certain file formats
+                   if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                       && $imageFileType != "gif"
+                   ) {
+                       echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                       $uploadOk = 0;
+                   }
+           // Check if $uploadOk is set to 0 by an error
+                   if ($uploadOk == 0) {
+                       echo "Sorry, your file was not uploaded.";
+           // if everything is ok, try to upload file
+                   } else {*/
 
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-            exec('telepath -r '.$base_target_file);
-            exec('rm -r '.$base_target_file.'/*');
-            echo "The file " . basename($_FILES["file"]["name"]) . " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
+            if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+//            exec('telepath -r ' . $base_target_file);
+//            exec('rm -r ' . $base_target_file . '/*');
+                echo "The file " . basename($_FILES["file"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+
         }
-    }
+
+
 
 
 }
