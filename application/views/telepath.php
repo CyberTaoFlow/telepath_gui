f2d28a440284ce05f12a6b2e4a79c8fea6f7499c
Yossi Cahen <yossi@ravtech.co.il>
2016-05-25 12:57:13 +0300

    Minify JS and CSS (continuation)

--- application/views/telepath.php
+++ application/views/telepath.php
@@ -2,18 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $is_admin = $this->ion_auth->is_admin();
-$perms    = array();
+$perms = array();
 
-if(!$is_admin) {
-	$this->acl->init_current_acl();
-	$tmp = $this->acl->perms;
-	
-	if(!empty($tmp)) {
-		foreach($tmp as $k => $v) {
-			$perms[str_replace('::', '_', $k)] = true;
-		}
-	}
-	
+if (!$is_admin) {
+    $this->acl->init_current_acl();
+    $tmp = $this->acl->perms;
+
+    if (!empty($tmp)) {
+        foreach ($tmp as $k => $v) {
+            $perms[str_replace('::', '_', $k)] = true;
+        }
+    }
+
 }
 
 /*
@@ -52,127 +52,78 @@ if($logged_in && $can_access) {
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<title>Telepath 3.0</title>
-	<link rel='shortcut icon' href='/favicon.ico'/>
-	<link rel='icon' href='/favicon.ico'/>
+    <meta charset="utf-8">
+    <title>Telepath 3.0</title>
+    <link rel='shortcut icon' href='/favicon.ico'/>
+    <link rel='icon' href='/favicon.ico'/>
 
-	<?php
+    <?php
 
-	if ($_SERVER['HTTP_HOST'] == 'localhost') {
-		$rebuild=true;
-	}
-	else{
-		$rebuild=false;
-	}
-		// add css files
-		$this->minify->css(array('reset.css','/ui-lightness/jquery-ui-1.10.4.custom.min.css', 'telepath.css',
-			'listitem.css', 'infoblock.css', "icons.css", "flags.css", "overlay.css", "jquery.contextmenu.css", "slider.css", "tipsy.css"));
+    if ($_SERVER['HTTP_HOST'] == 'localhost') {
+        $rebuild = true;
+    } else {
+        $rebuild = false;
+    }
 
-		// bool argument for rebuild css (false means skip rebuilding).
-		echo $this->minify->deploy_css($rebuild);
+    $css_files = array('reset.css', 'ui-lightness/jquery-ui-1.10.4.custom.min.css', 'telepath.css', 'listitem.css', 'infoblock.css', "icons.css", "flags.css", "overlay.css", "jquery.contextmenu.css", "slider.css", "tipsy.css");
+    // add css files
+    $this->minify->css($css_files);
 
-	if ($_SERVER['HTTP_HOST'] == 'localhost'){
-	?>
-			<link rel="stylesheet" href="css/reset.css">
-			<link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.10.4.custom.min.css">
-			<link rel="stylesheet" href="css/telepath.css">
-			<link rel="stylesheet" href="css/listitem.css">
-			<link rel="stylesheet" href="css/infoblock.css">
-			<link rel="stylesheet" href="css/icons.css">
-			<link rel="stylesheet" href="css/flags.css">
-			<link rel="stylesheet" href="css/overlay.css">
-			<link rel="stylesheet" href="css/jquery.contextmenu.css">
-			<link rel="stylesheet" href="css/slider.css">
-			<link rel="stylesheet" href="css/tipsy.css">
-	<?php }?>
+    // bool argument for rebuild css (false means skip rebuilding).
+    echo $this->minify->deploy_css($rebuild);
 
+    if ($_SERVER['HTTP_HOST'] == 'localhost') {
+        foreach ($css_files as $src) {
+            echo '<link rel="stylesheet" href="css/' . $src . '">';
+        }
+    } ?>
 
-		<script>
-		var telepath = { 
-			basePath: "<?php echo dirname($_SERVER['PHP_SELF']) != '/' ? dirname($_SERVER['PHP_SELF']) : ''; ?>",
-			controllerPath: "<?php echo $_SERVER['PHP_SELF']; ?>",
-			licenseValid: "VALID",
-			access: <?php echo json_encode(array('admin' => $is_admin, 'perm' => $perms)) ?>
-		};
-	</script>
+    <script>
+        var telepath = {
+            basePath: "<?php echo dirname($_SERVER['PHP_SELF']) != '/' ? dirname($_SERVER['PHP_SELF']) : ''; ?>",
+            controllerPath: "<?php echo $_SERVER['PHP_SELF']; ?>",
+            licenseValid: "VALID",
+            access: <?php echo json_encode(array('admin' => $is_admin, 'perm' => $perms)) ?>
+        };
+    </script>
 
-	<?php
+    <?php
 
-			$autoload = array(
-			"/lib/jquery-1.11.0.min.js",
-			"/lib/jquery-ui-1.10.4.custom.min.js",
-			"/lib/yepnope.1.5.4-min.js",
-			"/lib/jquery.contextmenu.min.js",
-			"/lib/jquery.fileupload.min.js",
-			"/lib/jquery.iframe-transport.min.js",
-			"/lib/jquery.flot.min.js",
-			"/lib/jquery.flot.resize.min.js",
-			"/lib/jquery.flot.pie.min.js",
-			"/lib/jquery.flot.selection.min.js",
-			"/lib/jquery.flot.time.min.js",
-			"/lib/jquery.flot.axislabels.min.js",
-			"/lib/bootstrap-slider.min.js",
-			"/lib/jquery.flot.symbol.min.js",
-			"/lib/jquery.flot.tooltip.min.js",
-			"/lib/jquery.tipsy.min.js"
-		);
+    $autoload = array(
+        "lib/jquery-1.11.0.min.js",
+        "lib/jquery-ui-1.10.4.custom.min.js",
+        "lib/yepnope.1.5.4-min.js",
+        "lib/jquery.contextmenu.min.js",
+        "lib/jquery.fileupload.min.js",
+        "lib/jquery.iframe-transport.min.js",
+        "lib/jquery.flot.min.js",
+        "lib/jquery.flot.resize.min.js",
+        "lib/jquery.flot.pie.min.js",
+        "lib/jquery.flot.selection.min.js",
+        "lib/jquery.flot.time.min.js",
+        "lib/jquery.flot.axislabels.min.js",
+        "lib/bootstrap-slider.min.js",
+        "lib/jquery.flot.symbol.min.js",
+        "lib/jquery.flot.tooltip.min.js",
+        "lib/jquery.tipsy.min.js"
+    );
 
+    array_push($autoload, "telepath.js", "telepath.header.js", "telepath.utils.js", "telepath.ds.js", "telepath.infoblock.js", "telepath.popup.js", "telepath.countries.js", "telepath.dropdown.js", "telepath.radios.js", "telepath.button.js", "telepath.checkbox.js", "telepath.search.js", "telepath.listitem.js", "telepath.listitem.generic.js", "telepath.toggle.js", "telepath.daterange.js", "telepath.graph.js", "telepath.vectormap.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.notifications.js", "telepath.notifications.js", "telepath.overlay.js", "telepath.pagination.js", "telepath.config.js", "telepath.dashboard.js", "telepath.case.js", "telepath.cases.js", "telepath.alert.js", "telepath.alerts.js", "telepath.suspects.js", "telepath.reports.js");
 
+    // add js files
+    $this->minify->js($autoload);
 
+    // rebuild js (false means skip rebuilding).
+    echo $this->minify->deploy_js($rebuild);
 
-		array_push($autoload,"telepath.js", "telepath.header.js", "telepath.utils.js", "telepath.ds.js", "telepath.infoblock.js","telepath.popup.js", "telepath.countries.js", "telepath.dropdown.js", "telepath.radios.js", "telepath.button.js", "telepath.checkbox.js", "telepath.search.js", "telepath.listitem.js", "telepath.listitem.generic.js", "telepath.toggle.js", "telepath.daterange.js", "telepath.graph.js", "telepath.vectormap.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.notifications.js", "telepath.notifications.js", "telepath.overlay.js", "telepath.pagination.js", "telepath.config.js", "telepath.dashboard.js", "telepath.case.js", "telepath.cases.js", "telepath.alert.js", "telepath.alerts.js", "telepath.suspects.js", "telepath.reports.js");
+    if ($_SERVER['HTTP_HOST'] == 'localhost') {
 
-	// add js files
-	$this->minify->js($autoload);
+        foreach ($autoload as $src) {
+            echo '<script src="js/' . $src . '"></script>';
+        }
+    }
+    ?>
 
-	// rebuild js (false means skip rebuilding).
-		echo $this->minify->deploy_js($rebuild);
-
-	if ($_SERVER['HTTP_HOST'] == 'localhost') {
-
-		foreach($autoload as $src) {
-			echo '<script src="js' . $src . '"></script>';
-		}
-		?>
-
-
-			<script src="js/telepath.js"></script>
-			<script src="js/telepath.header.js"></script>
-
-			<script src="js/telepath.utils.js"></script>
-			<script src="js/telepath.ds.js"></script>
-			<script src="js/telepath.infoblock.js"></script>
-			<script src="js/telepath.popup.js"></script>
-			<script src="js/telepath.countries.js"></script>
-			<script src="js/telepath.dropdown.js"></script>
-			<script src="js/telepath.radios.js"></script>
-			<script src="js/telepath.button.js"></script>
-			<script src="js/telepath.checkbox.js"></script>
-			<script src="js/telepath.search.js"></script>
-			<script src="js/telepath.listitem.js"></script>
-			<script src="js/telepath.listitem.generic.js"></script>
-			<script src="js/telepath.toggle.js"></script>
-			<script src="js/telepath.daterange.js"></script>
-			<script src="js/telepath.graph.js"></script>
-			<script src="js/telepath.vectormap.js"></script>
-			<script src="js/telepath.anomalyscore.js"></script>
-			<script src="js/telepath.notifications.js"></script>
-			<script src="js/telepath.overlay.js"></script>
-			<script src="js/telepath.pagination.js"></script>
-
-			<script src="js/telepath.config.js"></script>
-
-			<script src="js/telepath.dashboard.js"></script>
-			<script src="js/telepath.case.js"></script>
-			<script src="js/telepath.cases.js"></script>
-			<script src="js/telepath.alert.js"></script>
-			<script src="js/telepath.alerts.js"></script>
-			<script src="js/telepath.suspects.js"></script>
-			<script src="js/telepath.reports.js"></script>
-	<?php 	}
-	?>
-	
 
 </head>
 
