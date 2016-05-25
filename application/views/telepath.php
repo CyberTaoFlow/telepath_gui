41948698705dbdcee4c4f6fb85d660ffe337246e
Yossi Cahen <yossi@ravtech.co.il>
2016-05-25 15:41:15 +0300

    Minify JS and CSS (continuation)

--- application/views/telepath.php
+++ application/views/telepath.php
@@ -60,23 +60,22 @@ if($logged_in && $can_access) {
     <?php
 
     if ($_SERVER['HTTP_HOST'] == 'localhost') {
-        $rebuild = true;
-    } else {
-        $rebuild = false;
-    }
+        $css_files = array('reset.css', 'ui-lightness/jquery-ui-1.10.4.custom.min.css', 'telepath.css', 'listitem.css', 'infoblock.css', "icons.css", "flags.css", "overlay.css", "jquery.contextmenu.css", "slider.css", "tipsy.css");
+        // add css files
+        $this->minify->css($css_files);
 
-    $css_files = array('reset.css', 'ui-lightness/jquery-ui-1.10.4.custom.min.css', 'telepath.css', 'listitem.css', 'infoblock.css', "icons.css", "flags.css", "overlay.css", "jquery.contextmenu.css", "slider.css", "tipsy.css");
-    // add css files
-    $this->minify->css($css_files);
+        // bool argument for rebuild css (false means skip rebuilding).
+        echo $this->minify->deploy_css(false);
 
-    // bool argument for rebuild css (false means skip rebuilding).
-    echo $this->minify->deploy_css($rebuild);
-
-    if ($_SERVER['HTTP_HOST'] == 'localhost') {
         foreach ($css_files as $src) {
             echo '<link rel="stylesheet" href="css/' . $src . '">';
         }
-    } ?>
+    } else {
+        echo '<link rel="stylesheet" href="css/styles.min.css">';
+    }
+
+    ?>
+
 
     <script>
         var telepath = {
@@ -88,39 +87,40 @@ if($logged_in && $can_access) {
     </script>
 
     <?php
-
-    $js_files = array(
-        "lib/jquery-1.11.0.min.js",
-        "lib/jquery-ui-1.10.4.custom.min.js",
-        "lib/yepnope.1.5.4-min.js",
-        "lib/jquery.contextmenu.min.js",
-        "lib/jquery.fileupload.min.js",
-        "lib/jquery.iframe-transport.min.js",
-        "lib/jquery.flot.min.js",
-        "lib/jquery.flot.resize.min.js",
-        "lib/jquery.flot.pie.min.js",
-        "lib/jquery.flot.selection.min.js",
-        "lib/jquery.flot.time.min.js",
-        "lib/jquery.flot.axislabels.min.js",
-        "lib/bootstrap-slider.min.js",
-        "lib/jquery.flot.symbol.min.js",
-        "lib/jquery.flot.tooltip.min.js",
-        "lib/jquery.tipsy.min.js"
-    );
-
-    array_push($js_files, "telepath.js", "telepath.header.js", "telepath.utils.js", "telepath.ds.js", "telepath.infoblock.js", "telepath.popup.js", "telepath.countries.js", "telepath.dropdown.js", "telepath.radios.js", "telepath.button.js", "telepath.checkbox.js", "telepath.search.js", "telepath.listitem.js", "telepath.listitem.generic.js", "telepath.toggle.js", "telepath.daterange.js", "telepath.graph.js", "telepath.vectormap.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.notifications.js", "telepath.notifications.js", "telepath.overlay.js", "telepath.pagination.js", "telepath.config.js", "telepath.dashboard.js", "telepath.case.js", "telepath.cases.js", "telepath.alert.js", "telepath.alerts.js", "telepath.suspects.js", "telepath.reports.js");
-
-    // add js files
-    $this->minify->js($js_files);
-
-    // rebuild js (false means skip rebuilding).
-    echo $this->minify->deploy_js($rebuild);
-
     if ($_SERVER['HTTP_HOST'] == 'localhost') {
+
+        $js_files = array(
+            "lib/jquery-1.11.0.min.js",
+            "lib/jquery-ui-1.10.4.custom.min.js",
+            "lib/yepnope.1.5.4-min.js",
+            "lib/jquery.contextmenu.min.js",
+            "lib/jquery.fileupload.min.js",
+            "lib/jquery.iframe-transport.min.js",
+            "lib/jquery.flot.min.js",
+            "lib/jquery.flot.resize.min.js",
+            "lib/jquery.flot.pie.min.js",
+            "lib/jquery.flot.selection.min.js",
+            "lib/jquery.flot.time.min.js",
+            "lib/jquery.flot.axislabels.min.js",
+            "lib/bootstrap-slider.min.js",
+            "lib/jquery.flot.symbol.min.js",
+            "lib/jquery.flot.tooltip.min.js",
+            "lib/jquery.tipsy.min.js"
+        );
+
+        array_push($js_files, "telepath.js", "telepath.header.js", "telepath.utils.js", "telepath.ds.js", "telepath.infoblock.js", "telepath.popup.js", "telepath.countries.js", "telepath.dropdown.js", "telepath.radios.js", "telepath.button.js", "telepath.checkbox.js", "telepath.search.js", "telepath.listitem.js", "telepath.listitem.generic.js", "telepath.toggle.js", "telepath.daterange.js", "telepath.graph.js", "telepath.vectormap.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.anomalyscore.js", "telepath.notifications.js", "telepath.notifications.js", "telepath.overlay.js", "telepath.pagination.js", "telepath.config.js", "telepath.dashboard.js", "telepath.case.js", "telepath.cases.js", "telepath.alert.js", "telepath.alerts.js", "telepath.suspects.js", "telepath.reports.js");
+
+        // add js files
+        $this->minify->js($js_files);
+
+        // rebuild js (false means skip rebuilding).
+        echo $this->minify->deploy_js(false);
 
         foreach ($js_files as $src) {
             echo '<script src="js/' . $src . '"></script>';
         }
+    } else {
+        echo '<script src="js/scripts.min.js"></script>';
     }
     ?>
 
