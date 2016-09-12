af4730c53b8259707566adbb646f9b5101d2c9ec
Yossi Cahen <yossi@ravtech.co.il>
2016-09-12 16:41:59 +0300

    ALERTS - PERFORMANCE - separate server requests in 3 threads (continuation)

--- application/controllers/alerts.php
+++ application/controllers/alerts.php
@@ -15,81 +15,6 @@ class Alerts extends Tele_Controller
         $this->apps = $this->_get_apps();
     }
 
-    public function index()
-    {
-
-        $sort = $this->input->post('sort');
-        $dir = $this->input->post('dir') == 'true' ? 'ASC' : 'DESC';
-        $search = $this->input->post('search');
-        $alerts_filter = $this->input->post('alertsFilter');
-        $actions_filter = $this->input->post('actionsFilter');
-//        if ($search && substr($search, -1) != '*' && $search[0]!='"' && substr($search, -1) != '"' && strpos($search, 'country_code') !== 0)
-//        {
-//            $search = str_replace('OR*','OR',str_replace('AND*','AND',str_replace(' ','* ',$search))) . '*';
-//
-//        }
-//        $offset = intval($this->input->post('offset')) > 0 ? intval($this->input->post('offset')) : 0;
-
-        $displayed = $this->input->post('displayed');
-
-        if (!$sort || !in_array($sort, array('date', 'name', 'count', 'score'))) {
-            $sort = 'date';
-        }
-
-        $range = $this->_get_range();
-        $apps = $this->_get_apps();
-
-
-        // Prep filter
-//        $filter = array();
-
-        $actions_sid=[];
-        //Action filter
-        if (count($actions_filter) > 0 && $actions_filter != false) {
-
-            $actions_sid = $this->M_Alerts->get_action_filter($actions_filter, $range, $search, $apps);
-        }
-
-        // Alerts Data
-        $alerts = $this->M_Alerts->get_alerts($sort, $dir, $displayed, 15, $range, $apps, $search, $alerts_filter, $actions_sid);
-
-        if ( $displayed ) {
-            // We need just the alert items
-            return_json($alerts);
-        }
-
-        $time_chart = $this->M_Alerts->get_time_chart($range, $apps, $search, $alerts_filter, $actions_sid);
-        $distribution_chart = $this->M_Alerts->get_distribution_chart($range, $apps, $search);
-        $action_distribution_chart = $this->M_Alerts->get_action_distribution_chart($range, $apps, $search, $alerts_filter);
-
-
-        if ($alerts_filter){
-            foreach($distribution_chart as $key => $dis){
-                if (!in_array($dis['label'], $alerts_filter)){
-                    $distribution_chart[$key]['data']=0;
-                }
-            }
-        }
-        if ($actions_filter){
-            foreach($action_distribution_chart as $key => $dis){
-                if (!in_array($dis['label'], $actions_filter)){
-                    $action_distribution_chart[$key]['data']=0;
-                }
-            }
-        }
-
-
-        return_success(
-            array(
-                'alerts' => $alerts,
-                'time_chart' => $time_chart,
-                'distribution_chart' => $distribution_chart,
-                'action_distribution_chart' => $action_distribution_chart
-            )
-        );
-
-    }
-
     public function get_alerts()
     {
 
@@ -107,17 +32,16 @@ class Alerts extends Tele_Controller
         $range = $this->range;
         $apps = $this->apps;
 
-        $actions_sid=[];
+        $actions_sid = [];
         //Action filter
         if (count($actions_filter) > 0 && $actions_filter != false) {
 
             $actions_sid = $this->M_Alerts->get_action_filter($actions_filter, $range, $search, $apps);
         }
 
-        $alerts = $this->M_Alerts->get_alerts($sort, $dir, $displayed, 15, $range, $apps, $search, $alerts_filter,
-            $actions_sid);
+        $alerts = $this->M_Alerts->get_alerts($sort, $dir, $displayed, 15, $range, $apps, $search, $alerts_filter, $actions_sid);
 
-        if ( $displayed ) {
+        if ($displayed) {
             // We need just the alert items
             return_json($alerts);
         }
@@ -127,7 +51,7 @@ class Alerts extends Tele_Controller
 
     }
 
-   
+// Time chart and Alert filter
     public function get_charts()
     {
 
@@ -138,7 +62,7 @@ class Alerts extends Tele_Controller
         $range = $this->range;
         $apps = $this->apps;
 
-        $actions_sid=[];
+        $actions_sid = [];
         //Action filter
         if (count($actions_filter) > 0 && $actions_filter != false) {
 
@@ -157,12 +81,14 @@ class Alerts extends Tele_Controller
             }
         }
 
-        return_success(['time_chart' =>$time_chart,
-            'distribution_chart' =>$distribution_chart]);
+        return_success([
+            'time_chart' => $time_chart,
+            'distribution_chart' => $distribution_chart
+        ]);
 
     }
 
-
+// BA filter data
     public function get_action_distribution_chart()
     {
 
@@ -184,13 +110,8 @@ class Alerts extends Tele_Controller
             }
         }
 
-        return_success(['action_distribution_chart' =>$action_distribution_chart]);
+        return_success(['action_distribution_chart' => $action_distribution_chart]);
     }
 
-
-
-
-
-    
 
 }
